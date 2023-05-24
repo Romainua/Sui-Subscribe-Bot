a78ba7b620181499100711ac110837a8c0434944
Romainua <romanosadchiy458@gmail.com>
2023-05-24 14:02:54 +0200

    update examples

--- pkg/text/text.go
+++ pkg/text/text.go
@@ -5,48 +5,26 @@ var Start string
 
 func Init_text() {
 	Example = `
-List of filter subscribe example (currently on devnet network):
+List of filter subscribe example (currently on mainnet network):
 
-Subscribe to Move Event via Package 0x2 and Module devnet_nft
-{"jsonrpc":"2.0", "id": 1, "method": "sui_subscribeEvent", "params": [{"All":[{"EventType":"MoveEvent"}, {"Package":"0x2"}, {"Module":"devnet_nft"}]}]}
-  
-Subscribe to only Publish activity
-{"jsonrpc":"2.0", "id": 1, "method": "sui_subscribeEvent", "params": [{"All":[{"EventType":"Publish"}]}]}
-   
 Subscribe to all activity in network (too many tx)
-{"jsonrpc":"2.0", "id": 1, "method": "sui_subscribeEvent", "params": [{"All":[]}]}
+{"jsonrpc":"2.0", "id": 1, "method": "suix_subscribeEvent", "params": [{"All":[]}]}
 
 Subscribe to all activity from address
-{"jsonrpc":"2.0", "id": 1, "method": "sui_subscribeEvent", "params": [{"All":[{"SenderAddress": "0xc4173a804406a365e69dfb297d4eaaf002546ebd"}]}]}
+{"jsonrpc":"2.0", "id": 1, "method": "suix_subscribeEvent", "params": [{"All":[{"Sender": "0xb7847468db546ba85acb9dcdc0c5190b3ca6427d713ff52a4f8183c81f8a39e1"}]}]}
 
-Subscribe to activity from address which include only coin balance changing event
-{"jsonrpc":"2.0", "id": 1, "method": "sui_subscribeEvent", "params": [{"All":[{"SenderAddress":"0xc4173a804406a365e69dfb297d4eaaf002546ebd"},{"EventType":"CoinBalanceChange"}]}]}
+Suscribe to Staking Events
+{"jsonrpc":"2.0", "id": 1, "method": "suix_subscribeEvent", "params": [{"MoveEventType": "0x3::validator::StakingRequestEvent"}]}
 
-Suscribe to all activity which include Transfer Object
-{"jsonrpc":"2.0", "id": 1, "method": "sui_subscribeEvent", "params": [{"All":[{"EventType":"TransferObject"}]}]}
-
-Suscribe to only Transfer Object activity from address
-{"jsonrpc":"2.0", "id": 1, "method": "sui_subscribeEvent", "params": [{"All":[{"EventType":"TransferObject"}, {"SenderAddress": "0xc4173a804406a365e69dfb297d4eaaf002546ebd"}]}]}
-
-check activity of your package, just replace Package to your
-{"jsonrpc":"2.0", "id": 1, "method": "sui_subscribeEvent", "params": [{"All":[{"EventType":"MoveEvent"},{"Package":"0xc8832b8cddf246f3fc010c10f9d62d89c163d8cc"}]}]}
-
-check activity of your module, just replace Module to your
-{"jsonrpc":"2.0", "id": 1, "method": "sui_subscribeEvent", "params": [{"All":[{"Module":"game_8192"}]}]}
-
-check activity of couple Modules
-{"jsonrpc":"2.0", "id": 1, "method": "sui_subscribeEvent", "params": [{"Any":[{"Module":"game_8192"}, {"Module":"devnet_nft"}]}]}
+Suscribe to Staking Events by custom validator name
+{"jsonrpc":"2.0", "id": 1, "method": "suix_subscribeEvent", "params": [{"And":[{"MoveEventField": {"path":"/validator_address","value":"0x1290ab8bca1c136d2b86967674a43a0f5cb30b352f90233cc37ebf7452b96787"}},{"MoveEventType": "0x3::validator::StakingRequestEvent"}]}]}
 
 if you need any help to create your own filter feel free dming to @romanosadchyi Thanks!`
 
 	Start = `
-Hi, User! Just send json requsts via your filters like (currently on devnet network):
-				
-     {"jsonrpc":"2.0", "id": 1, "method": "sui_subscribeEvent", "params": [{"All":[{"EventType":"MoveEvent"}, {"Package":"0x2"}, {"Module":"devnet_nft"}]}]}
-     
-     {"jsonrpc":"2.0", "id": 1, "method": "sui_subscribeEvent", "params": [{"All":[{"EventType":"Publish"}]}]}
-     
-     {"jsonrpc":"2.0", "id": 1, "method": "sui_subscribeEvent", "params": [{"All":[]}]}
+Hi, User! Just send json requsts via your filters like (currently on mainnet network):
+
+     {"jsonrpc":"2.0", "id": 1, "method": "suix_subscribeEvent", "params": [{"All":[]}]}
 	
 Input /example to check more example.
 More info are there https://docs.sui.io/build/event_api#event-filters`
